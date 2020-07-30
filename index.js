9cbbb57d2b3f4be72007563d861ebaf07da0d18b
Raul Figueroa <raul.a.figueroa@hotmail.com>
2020-07-30 15:54:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function findMinAndRemove(array){
   let min;
+  let newIndex;
   for (let i = 0; i < array.length; i++) {
     if (i < array[0]) {
       min = array[i]
     }
   }
+  array.splice(min, 1)
   return min
 }
 
